cd9a8b4626e98db9dfe4bb41c7e1e37e777c5505
pihusingla8 <pihusingla8@gmail.com>
2024-08-10 11:30:30 +0100

    temperature converter

--- chapter1.py
+++ chapter1.py
@@ -1,1 +1,20 @@
-print("Hello World!")
+import math
+#print("Hello World!")
+
+def convertToCelsius(tempInFarhenheit):
+    tempInCelcius = (tempInFarhenheit - 32) * 5 / 9
+    return tempInCelcius
+    
+
+tempInFarhenheit = float(input())
+celcius = convertToCelsius(tempInFarhenheit)
+print(celcius)
+
+
+def converttoFahrenheit(tempInCelcius):
+    tempInFarhenheit = tempInCelcius * (9 / 5) + 32
+    return tempInFarhenheit
+
+tempInCelsius = float(input())
+farhenheit = converttoFahrenheit(tempInCelsius)
+print(farhenheit)
