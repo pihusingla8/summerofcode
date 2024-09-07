b8b63307701295a1ee6f67a46cf47526227e666c
pihusingla8 <pihusingla8@gmail.com>
2024-09-07 12:45:02 +0100

    cars 3_3

--- friends3.1.py
+++ friends3.1.py
@@ -3,3 +3,4 @@ print(friends)
 print(friends[0] + ' is my friend')
 print(friends[1] + ' is my friend')
 print(friends[2] + ' is my friend')
+
---
+++ transportations3_3.py
@@ -0,0 +1,5 @@
+cars = ['Audi', 'BMW', 'Tesla']
+print(cars)
+print('I have the ' + cars[0] + ' car')
+print('I like the ' + cars[1] + ' car')
+print(cars[2] + ' is a good car')
