a8ae6ec5d5ee41d83342b42cb5b202662f5ddb3c
pihusingla8 <pihusingla8@gmail.com>
2024-09-07 13:12:01 +0100

    pizassss! 4_1

--- friends3.1.py
+++ friends3.1.py
@@ -1,5 +1,7 @@
-friends = ['Jasmine', 'Prisha', 'Krishita']
+friends = ['Jasmine', 'Prisha', 'Krishita', 'Amelia', 'Ariana', 'Naina']
 print(friends)
+for friend in friends :
+    print(friend)
 print(friends[0] + ' is my friend')
 print(friends[1] + ' is my friend')
 print(friends[2] + ' is my friend')
---
+++ pizzas4_1.py
@@ -0,0 +1,5 @@
+pizzas = ['cheese pizza', 'vegetable pizza', 'veg pepperoni pizza']
+print(pizzas)
+for pizzas in pizzas :
+    print('I like this ' + pizzas)
+print('Pizzas are my life!!!!!')
