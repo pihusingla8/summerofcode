076c321a13bf78ad4d92694927e3885ecc743d11
pihusingla8 <pihusingla8@gmail.com>
2024-09-07 13:32:53 +0100

    sea creatures 4_2

---
+++ animals4_2.py
@@ -0,0 +1,5 @@
+animals = ['Sharks', 'Dolphins', 'Whales']
+print(animals)
+for animal in animals :
+    print(animal + ' has a tail and is grey a colour')
+print('All the animals are a sea creature!')
